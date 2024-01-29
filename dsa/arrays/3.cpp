19fc3863bab3488494ededc68c13ac310d4d84a2
Sagar Yadav <sagardxd5@gmail.com>
2024-01-29 20:49:24 +0530

    added 3.cpp

---
+++ dsa/arrays/3.cpp
@@ -0,0 +1,15 @@
+// Coding ninja - Find Duplicate in Array 
+// link- https://www.codingninjas.com/studio/problems/find-duplicate-in-array_1112602
+
+#include <bits/stdc++.h>
+
+int findDuplicate(vector<int> &arr, int n){
+	// Write your code here.
+	sort(arr.begin(), arr.end());
+
+	for(int i=0; i<n-1; i++) {
+		if(arr[i] == arr[i+1]){
+			return arr[i];
+		}
+	}
+}
